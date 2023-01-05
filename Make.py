c9fba93858fb00b3e99e7cfee127e786d8d18257
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2023-01-04 22:44:02 -0600

    Add files via upload

---
+++ Make.py
@@ -0,0 +1,30 @@
+#OUTLINE**************
+#Add ingredients
+#Search for possible recipes
+#Print list of recipes possible from ingredients present
+#User inputs chosen recipe
+#Print user chosen Recipe
+
+#Adding ingredients
+g = {}
+g.update({'eggs': 10})
+g.update({'Bacon slices': 19})
+
+#Search for possible recipes
+print(g)
+
+#Print list of recipes possible from ingredients present
+
+
+#User inputs chosen recipe
+
+
+#Print user chosen Recipe
+
+
+
+#Printing the recipe
+#fh = open("Eggs and Bacon.txt")
+#count = 0
+#for x in fh :
+    #print(x)
