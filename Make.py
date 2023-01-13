4c787a8f193e433a9732db477548c8926242d062
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2023-01-13 15:21:25 -0600

    Add files via upload

--- Make.py
+++ Make.py
@@ -9,18 +9,33 @@
 g = {}
 g.update({"eggs": 10})
 g.update({"bacon slices": 19})
+g.update({"bread": 20})
+g.update({"lettuce": 5})
+g.update({"tomato": 2})
+g.update({"mayonaise": 30})
 
 #Search for possible recipes
 eggs = g.get("eggs")
 bacon = g.get("bacon slices")
+bread = g.get("bread")
+lettuce = g.get("lettuce")
+tomato = g.get("tomato")
+mayo = g.get("mayonaise")
 
 def breakfast():
     if eggs >= 3 and bacon >= 3:
         print("You can make eggs and bacon!")
     else:
-        print("You need to get groceries")
+        pass
+
+def BLT():
+    if bread >= 2 and bacon >= 2 and lettuce >= 1 and tomato >= 1 and mayo >= 1:
+        print("You can make a BLT!")
+    else:
+        pass
 
 breakfast()
+BLT()
 
 
 #Print list of recipes possible from ingredients present
